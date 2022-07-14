4ee836cb52aa73e7060c20afa432bb971c3a8623
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-14 21:36:54 +1000

    starting working in layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import MainLayout from "./layout/MainLayout";
 import EntryPage from "./pages/entry/EntryPage";
 
 const App = () => {
   return (
     <div>
+      <MainLayout />
       <EntryPage />
     </div>
   );
