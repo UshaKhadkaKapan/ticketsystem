2c023b2bdbbc1ba274e850bfdc2fb3c096623b03
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-14 22:16:38 +1000

    complete header part

--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../../src/assets/logo.PNG";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Navbar bg="info" variant="dark" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="logo" width="100px" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="/ticket">Ticket</Nav.Link>
+              <Nav.Link href="/logout">Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
